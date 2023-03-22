1415d9a9c1992a25fcbaed7875bc301f79cf7062
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2023-03-23 00:00:32 +0100

    Update injector.config.dart

--- lib/src/services/injector/injector.config.dart
+++ lib/src/services/injector/injector.config.dart
@@ -5,22 +5,24 @@
 // **************************************************************************
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:dio/dio.dart' as _i4;
-import 'package:extchange/src/bloc/rates_series/rates_series_bloc.dart' as _i9;
-import 'package:extchange/src/bloc/rates_tables/rates_tables_bloc.dart' as _i10;
+import 'package:dio/dio.dart' as _i5;
+import 'package:extchange/src/bloc/rates_series/rates_series_bloc.dart' as _i10;
+import 'package:extchange/src/bloc/rates_tables/rates_tables_bloc.dart' as _i11;
 import 'package:extchange/src/services/data/clients/rates_series_client.dart'
-    as _i5;
-import 'package:extchange/src/services/data/clients/rates_tables_client.dart'
-    as _i7;
-import 'package:extchange/src/services/data/config.dart' as _i3;
-import 'package:extchange/src/services/data/repos/rates_series_repo.dart'
     as _i6;
-import 'package:extchange/src/services/data/repos/rates_tables_repo.dart'
+import 'package:extchange/src/services/data/clients/rates_tables_client.dart'
     as _i8;
+import 'package:extchange/src/services/data/config.dart' as _i4;
+import 'package:extchange/src/services/data/repos/rates_series_repo.dart'
+    as _i7;
+import 'package:extchange/src/services/data/repos/rates_tables_repo.dart'
+    as _i9;
 import 'package:get_it/get_it.dart' as _i1;
+import 'package:go_router/go_router.dart' as _i3;
 import 'package:injectable/injectable.dart' as _i2;
 
-import '../data/dio_provider.dart' as _i11;
+import '../data/dio_provider.dart' as _i12;
+import '../navigation/router.dart' as _i13;
 
 const String _prod = 'prod';
 
@@ -37,36 +39,40 @@ _i1.GetIt $initGetIt(
     environment,
     environmentFilter,
   );
+  final routerInjectionModule = _$RouterInjectionModule();
   final dioProvider = _$DioProvider();
-  gh.factory<_i3.IConfig>(() => _i3.AppConfig());
-  gh.singleton<_i4.Dio>(dioProvider.dio(gh<_i3.IConfig>()));
-  gh.factory<_i5.RatesSeriesClient>(
-    () => _i5.ProdRatesSeriesClient(
-      dio: gh<_i4.Dio>(),
-      config: gh<_i3.IConfig>(),
+  gh.singleton<_i3.GoRouter>(routerInjectionModule.router);
+  gh.factory<_i4.IConfig>(() => _i4.AppConfig());
+  gh.singleton<_i5.Dio>(dioProvider.dio(gh<_i4.IConfig>()));
+  gh.factory<_i6.RatesSeriesClient>(
+    () => _i6.ProdRatesSeriesClient(
+      dio: gh<_i5.Dio>(),
+      config: gh<_i4.IConfig>(),
     ),
     registerFor: {_prod},
   );
-  gh.factory<_i6.RatesSeriesRepo>(
-    () => _i6.ProdRatesSeries(client: gh<_i5.RatesSeriesClient>()),
+  gh.factory<_i7.RatesSeriesRepo>(
+    () => _i7.ProdRatesSeries(client: gh<_i6.RatesSeriesClient>()),
     registerFor: {_prod},
   );
-  gh.factory<_i7.RatesTablesClient>(
-    () => _i7.ProdRatesTablesClient(
-      dio: gh<_i4.Dio>(),
-      config: gh<_i3.IConfig>(),
+  gh.factory<_i8.RatesTablesClient>(
+    () => _i8.ProdRatesTablesClient(
+      dio: gh<_i5.Dio>(),
+      config: gh<_i4.IConfig>(),
     ),
     registerFor: {_prod},
   );
-  gh.factory<_i8.RatesTablesRepo>(
-    () => _i8.ProdRatesTables(client: gh<_i7.RatesTablesClient>()),
+  gh.factory<_i9.RatesTablesRepo>(
+    () => _i9.ProdRatesTables(client: gh<_i8.RatesTablesClient>()),
     registerFor: {_prod},
   );
-  gh.factory<_i9.RatesSeriesBloc>(
-      () => _i9.RatesSeriesBloc(gh<_i6.RatesSeriesRepo>()));
-  gh.factory<_i10.RatesTablesBloc>(
-      () => _i10.RatesTablesBloc(gh<_i8.RatesTablesRepo>()));
+  gh.factory<_i10.RatesSeriesBloc>(
+      () => _i10.RatesSeriesBloc(gh<_i7.RatesSeriesRepo>()));
+  gh.factory<_i11.RatesTablesBloc>(
+      () => _i11.RatesTablesBloc(gh<_i9.RatesTablesRepo>()));
   return getIt;
 }
 
-class _$DioProvider extends _i11.DioProvider {}
+class _$DioProvider extends _i12.DioProvider {}
+
+class _$RouterInjectionModule extends _i13.RouterInjectionModule {}
