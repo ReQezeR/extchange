62c9a3bbf83403d4d658f15d2cd54f76a56152ad
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2023-03-23 00:00:53 +0100

    add router navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,15 +1,23 @@
+import 'package:extchange/src/bloc/_index.dart';
+import 'package:extchange/src/services/injector/injector.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    final router = getIt<GoRouter>();
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => getIt<RatesSeriesBloc>()),
+        BlocProvider(create: (_) => getIt<RatesTablesBloc>()),
+      ],
+      child: MaterialApp.router(
+        title: 'Extchange',
+        routerConfig: router,
       ),
     );
   }
