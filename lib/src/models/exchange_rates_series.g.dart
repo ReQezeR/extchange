7e4c752ccbe52c3d9529eef4352a2486c7900f20
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2023-03-23 15:26:29 +0100

    update exchange rates series model

--- lib/src/models/exchange_rates_series.g.dart
+++ lib/src/models/exchange_rates_series.g.dart
@@ -11,13 +11,49 @@ ExchangeRatesSeries _$ExchangeRatesSeriesFromJson(Map<String, dynamic> json) =>
       table: json['table'] as String,
       currency: json['currency'] as String,
       code: json['code'] as String,
-      rates: (json['rates'] as List<dynamic>)
-          .map((e) => Rate.fromJson(e as Map<String, dynamic>))
-          .toList(),
     );
 
 Map<String, dynamic> _$ExchangeRatesSeriesToJson(
         ExchangeRatesSeries instance) =>
+    <String, dynamic>{
+      'table': instance.table,
+      'currency': instance.currency,
+      'code': instance.code,
+    };
+
+AvgExchangeRatesSeries _$AvgExchangeRatesSeriesFromJson(
+        Map<String, dynamic> json) =>
+    AvgExchangeRatesSeries(
+      table: json['table'] as String,
+      currency: json['currency'] as String,
+      code: json['code'] as String,
+      rates: (json['rates'] as List<dynamic>)
+          .map((e) => AvgRate.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$AvgExchangeRatesSeriesToJson(
+        AvgExchangeRatesSeries instance) =>
+    <String, dynamic>{
+      'table': instance.table,
+      'currency': instance.currency,
+      'code': instance.code,
+      'rates': instance.rates.map((e) => e.toJson()).toList(),
+    };
+
+BidAskExchangeRatesSeries _$BidAskExchangeRatesSeriesFromJson(
+        Map<String, dynamic> json) =>
+    BidAskExchangeRatesSeries(
+      table: json['table'] as String,
+      currency: json['currency'] as String,
+      code: json['code'] as String,
+      rates: (json['rates'] as List<dynamic>)
+          .map((e) => BidAskRate.fromJson(e as Map<String, dynamic>))
+          .toList(),
+    );
+
+Map<String, dynamic> _$BidAskExchangeRatesSeriesToJson(
+        BidAskExchangeRatesSeries instance) =>
     <String, dynamic>{
       'table': instance.table,
       'currency': instance.currency,
