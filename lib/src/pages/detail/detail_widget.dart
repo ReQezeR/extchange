82702d8f8c7e8483ce1b831d5761e59780642c39
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2023-03-30 17:53:38 +0200

    Update detail_widget.dart

--- lib/src/pages/detail/detail_widget.dart
+++ lib/src/pages/detail/detail_widget.dart
@@ -1,5 +1,6 @@
 import 'package:auto_animated/auto_animated.dart';
 import 'package:extchange/src/models/exchange_rates_series.dart';
+import 'package:extchange/src/pages/detail/line_chart_detail_widget.dart';
 import 'package:extchange/src/widgets/changes_icon.dart';
 import 'package:extchange/src/widgets/currency_icon.dart';
 import 'package:recase/recase.dart';
@@ -92,177 +93,167 @@ class _DetailWidgetState extends State<DetailWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        AnimateIfVisible(
-          key: const Key('item.0'),
-          duration: const Duration(milliseconds: 300),
-          builder: (
-            BuildContext context,
-            Animation<double> animation,
-          ) =>
-              buildAnimatedItem(
-            context,
-            animation,
-            child: Container(
-              constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height * 0.25,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(5)),
-              ),
-              // margin: const EdgeInsets.symmetric(vertical: 20),
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Container(
-                    height: MediaQuery.of(context).size.height * 0.10,
-                    margin: const EdgeInsets.all(10),
-                    // color: Colors.amber,
-                    child: Row(
-                      children: [
-                        Container(
-                          width: MediaQuery.of(context).size.height * 0.10,
-                          height: MediaQuery.of(context).size.height * 0.10,
-                          decoration: BoxDecoration(
-                            color: Colors.grey.shade800,
-                            borderRadius: const BorderRadius.all(Radius.circular(5)),
+        Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.25,
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+          // margin: const EdgeInsets.symmetric(vertical: 20),
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.10,
+                margin: const EdgeInsets.all(10),
+                // color: Colors.amber,
+                child: Row(
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.height * 0.10,
+                      height: MediaQuery.of(context).size.height * 0.10,
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade800,
+                        borderRadius: const BorderRadius.all(Radius.circular(5)),
+                      ),
+                      child: Center(
+                        child: CurrencyIcon(currencyCode: widget.avgSeries.code, size: 50, color: Colors.lime),
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Text(
+                              ReCase(widget.avgSeries.currency).titleCase,
+                              style: const TextStyle(
+                                fontSize: 30,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
                           ),
-                          child: Center(
-                            child: CurrencyIcon(currencyCode: widget.avgSeries.code, size: 50, color: Colors.lime),
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.max,
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            crossAxisAlignment: CrossAxisAlignment.start,
+                          Row(
                             children: [
-                              Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                                child: Text(
-                                  ReCase(widget.avgSeries.currency).titleCase,
-                                  style: const TextStyle(
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.w500,
-                                  ),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10.0),
+                                child: Icon(
+                                  Icons.date_range,
+                                  size: 14,
                                 ),
                               ),
-                              Row(
-                                children: [
-                                  const Padding(
-                                    padding: EdgeInsets.symmetric(horizontal: 10.0),
-                                    child: Icon(
-                                      Icons.date_range,
-                                      size: 14,
-                                    ),
-                                  ),
-                                  Text(
-                                    widget.avgSeries.rates[1].effectiveDate,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w500,
-                                    ),
-                                  ),
-                                ],
+                              Text(
+                                widget.avgSeries.rates[1].effectiveDate,
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500,
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 50,
-                    width: MediaQuery.of(context).size.width,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        getRate(title: "Kupno", rate: "${widget.bidAskSeries.rates[0].bidValue} PLN"),
-                        getRate(title: "Sprzedaż", rate: "${widget.bidAskSeries.rates[0].askValue} PLN"),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        const Divider(),
-        AnimateIfVisible(
-          key: const Key('item.1'),
-          duration: const Duration(milliseconds: 300),
-          builder: (
-            BuildContext context,
-            Animation<double> animation,
-          ) =>
-              buildAnimatedItem(
-            context,
-            animation,
-            child: Container(
-              constraints: BoxConstraints(
-                maxHeight: MediaQuery.of(context).size.height * 0.1,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.all(Radius.circular(5)),
-              ),
-              clipBehavior: Clip.antiAlias,
-              child: Column(
-                children: [
-                  Expanded(
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      height: MediaQuery.of(context).size.height * 0.1,
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Container(
-                            margin: const EdgeInsets.symmetric(horizontal: 0.0),
-                            height: MediaQuery.of(context).size.height * 0.1,
-                            child: const Center(
-                              child: Padding(
-                                padding: EdgeInsets.symmetric(horizontal: 10.0),
-                                child: Text(
-                                  "Kurs średni:",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 20,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Expanded(
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Text(
-                                  "${widget.avgSeries.rates[1].midValue} PLN",
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.w500,
-                                    fontSize: 20.0,
-                                    color: Colors.black,
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          ChangesIcon(
-                            currentValue: widget.avgSeries.rates[1].midValue,
-                            previousValue: widget.avgSeries.rates[0].midValue,
-                            size: MediaQuery.of(context).size.height * 0.1,
-                          )
                         ],
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
+              SizedBox(
+                height: 50,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    getRate(title: "Kupno", rate: "${widget.bidAskSeries.rates[0].bidValue} PLN"),
+                    getRate(title: "Sprzedaż", rate: "${widget.bidAskSeries.rates[0].askValue} PLN"),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
+        const Divider(),
+      ],
+    );
+  }
+}
+
+class AvgRateWidget extends StatelessWidget {
+  const AvgRateWidget({Key? key, required this.avgSeries}) : super(key: key);
+  final AvgExchangeRatesSeries avgSeries;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height * 0.1,
+          ),
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
+          clipBehavior: Clip.antiAlias,
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  height: MediaQuery.of(context).size.height * 0.1,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 0.0),
+                        height: MediaQuery.of(context).size.height * 0.1,
+                        child: const Center(
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 10.0),
+                            child: Text(
+                              "Kurs średni:",
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Text(
+                              "${avgSeries.rates[1].midValue} PLN",
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 20.0,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      ChangesIcon(
+                        currentValue: avgSeries.rates[1].midValue,
+                        previousValue: avgSeries.rates[0].midValue,
+                        size: MediaQuery.of(context).size.height * 0.1,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(),
       ],
     );
   }
@@ -280,6 +271,11 @@ class FailureWidget extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Container(
+          padding: const EdgeInsets.all(20),
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.all(Radius.circular(5)),
+          ),
           child: Column(
             children: [
               const Icon(
